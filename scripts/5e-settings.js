ca7bab1ad487380991d1192ba0298a0cceffec9a
colton-howe <colton.howe@hotmail.ca>
2021-05-18 22:00:11 -0400

    Initial Commit

---
+++ scripts/5e-settings.js
@@ -0,0 +1,11 @@
+const FUMBLE_VALUE = 3;
+
+Hooks.on("ready", () => {
+  if(!game.modules.get('lib-wrapper')?.active && game.user.isGM) {
+    ui.notifications.error("Module Wound Tracker requires the 'libWrapper' module. Please install and activate it.");
+  }
+
+  libWrapper.register("5e-settings", 'game.dnd5e.dice.d20Roll', (wrapped, ...args) => {
+    return wrapped({fumble: FUMBLE_VALUE, ...args});
+  }, "WRAPPER");
+});
